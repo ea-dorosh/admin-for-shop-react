e9fa5dfe994008014d61356cceb3e485f08a8c79
Egor <dorosh.ea@yandex.ru>
2020-11-13 11:29:26 +0400

    Add header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./index.css"
 import Header from "../../components/Dashboard/Header"
 import Navigation from "../../components/Dashboard/Navigation";
+import Page from "../../components/Dashboard/Page";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <div className={'app-wrapper'}>
             <Header/>
             <Navigation/>
+            <Page/>
         </div>
     );
 }
---
+++ src/components/Dashboard/Page/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css"
+import Header from "./Header";
+
+
+function Page() {
+    return (
+        <div className={'page'}>
+            <div className={'page__header'}>
+                <Header/>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+export default Page;
