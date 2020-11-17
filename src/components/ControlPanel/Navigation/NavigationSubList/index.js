eaa8a50846b4a068421fed08e7e820bde19e30ba
Egor <dorosh.ea@yandex.ru>
2020-11-17 17:38:11 +0400

    Add react router dom

--- src/components/ControlPanel/Navigation/NavigationSubList/index.js
+++ src/components/ControlPanel/Navigation/NavigationSubList/index.js
@@ -1,13 +1,18 @@
 import React from "react";
+import {Link, withRouter} from 'react-router-dom';
 import "./index.scss"
 
 
 function NavigationSubList() {
     return (
         <ul className={'navigation-sub-list'}>
-            <li className={'navigation-sub-list__item'}>All Orders</li>
+            <li className={'navigation-sub-list__item'}>
+              <Link to='/all-offers'>
+                All Orders
+              </Link>
+            </li>
         </ul>
     );
 }
 
-export default NavigationSubList;
+export default withRouter(NavigationSubList);
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,15 +1,28 @@
 import React from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import "./index.scss"
 import Dashboard from "../Pages/Dashboard";
 
 
 
 function App() {
-    return (
-        <div className={'app-wrapper'}>
-          <Dashboard/>
-        </div>
-    );
+  return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <div className={'app-wrapper'}>
+              <Dashboard/>
+            </div>
+          </Route>
+          <Route exact path='/all-orders'>
+            <div className={'app-wrapper'}>
+              <Dashboard/>
+            </div>
+          </Route>
+        </Switch>
+      </BrowserRouter>
+
+  );
 }
 
 export default App;
